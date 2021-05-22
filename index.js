58e0240148644e719bd429fbe039cca60c963e32
Vitor Mugnol <vitormugnol10@gmail.com>
2021-05-22 18:20:08 -0300

    mais funções

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 
-const getBooks = (request, response) => {
+const getFuncionario = (request, response) => {
   pool.query('SELECT * FROM CrowdMeet.Funcionarios', (error, results) => {
     if (error) {
       throw error
@@ -18,27 +18,177 @@ const getBooks = (request, response) => {
   })
 }
 
-const addBook = (request, response) => {
-  const {author, title} = request.body
+const addEvento = (request, response) => {
+  const {tipo, subtipo, datahora, localizacao} = request.body
 
   pool.query(
-    'INSERT INTO books (author, title) VALUES ($1, $2)',
-    [author, title],
+    'insert into CrowdMeet.evento (tipoevento, subtipo, datahora, localizacao) values ($1, $2, $3, $4)',
+    [tipo, subtipo, datahora, localizacao],
     (error) => {
       if (error) {
         throw error
       }
-      response.status(201).json({status: 'success', message: 'Book added.'})
+      response.status(201).json({status: 'success', message: 'Evento adicionado.'})
     },
   )
 }
 
-app
-  .route('/funcionarios')
-  // GET endpoint
-  .get(getBooks)
-  // POST endpoint
-  .post(addBook)
+const getEvento = (request, response) => {
+  const {id} = request.body
+  if(id == null){
+    pool.query('SELECT * FROM CrowdMeet.Evento', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Evento where id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+const getLocalizacao = (request, response) => {
+  const {id} = request.body
+  if(id == null){
+    pool.query('SELECT * FROM CrowdMeet.Localizacao', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Localizacao where id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+
+const getTipo = (request, response) => {
+  const {id} = request.body
+  if(id == null){
+    pool.query('SELECT * FROM CrowdMeet.Tipoevento', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Tipoevento where id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+const getSubtipo = (request, response) => {
+  const {id} = request.body
+  if(id == null){
+    pool.query('SELECT * FROM CrowdMeet.Subtipoevento', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Subtipoevento where id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+const getDepartamento = (request, response) => {
+  const {id} = request.body
+  if(id == null){
+    pool.query('SELECT * FROM CrowdMeet.Departamento', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Departamento where id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+const getParticipante = (request, response) => {
+  const {evento, funcionario} = request.body
+  if(evento != null){
+    pool.query('SELECT * FROM CrowdMeet.Participantes where evento = $1', [evento], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else if(funcionario != null){
+    pool.query('SELECT * FROM CrowdMeet.Participantes where funcionario = $1', [funcionario], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  else{
+    pool.query('SELECT * FROM CrowdMeet.Participantes', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+}
+
+const addParticipante = (request, response) => {
+  const {evento, funcionario} = request.body
+  pool.query(
+    'insert into CrowdMeet.participantes (evento, funcionario) values ($1, $2)',
+    [evento, funcionario],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({status: 'success', message: 'Participante adicionado.'})
+    },
+  )
+}
+
+app.route('/funcionarios').get(getFuncionario)
+
+app.route('/eventos').post(addEvento).get(getEvento)
+
+app.route('/localizacao').get(getLocalizacao)
+
+app.route('/participante').get(getParticipante).post(addParticipante)
+
+app.route('/tipo').get(getTipo)
+
+app.route('/subtipo').get(getSubtipo)
+
+app.route('/departamento').get(getDepartamento)
 
 // Start server
 app.listen(process.env.PORT || 3002,  () => {
