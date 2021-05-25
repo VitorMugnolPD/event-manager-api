cc3794b291c1eb3a69fad3f4bd97b643a9905885
Vitor Mugnol <vitormugnol10@gmail.com>
2021-05-25 18:13:44 -0300

    mudanças

--- index.js
+++ index.js
@@ -20,16 +20,22 @@ const getFuncionario = (request, response) => {
 }
 
 const addEvento = (request, response) => {
-  const {tipoevento, subtipo, datahora, localizacao, idorganizador} = request.body
+  const {tipoevento, datahora, localizacao, idorganizador} = request.body
+
+  // console.log("aaa" + tipoevento)
+  // console.log("aaa" + datahora)
+  // console.log("aaa" + localizacao)
+  // console.log("aaa" + idorganizador)
 
   pool.query(
-    'insert into CrowdMeet.evento (tipoevento, subtipo, datahora, localizacao, idOrganizador) values ($1, $2, $3, $4, $5)',
-    [tipoevento, subtipo, datahora, localizacao, idorganizador],
-    (error) => {
+    'insert into CrowdMeet.evento (tipoevento, datahora, localizacao, idorganizador) values ($1, $2, $3, $4) returning *',
+    [tipoevento, datahora, localizacao, idorganizador],
+    (error, results) => {
       if (error) {
         throw error
       }
-      response.status(201).json({status: 'success', message: 'Evento adicionado.'})
+      response.status(200).json(results.rows)
+      //response.status(201).json({status: 'success', message: 'Evento adicionado.'})
     },
   )
 }
@@ -54,6 +60,27 @@ const deleteEvento = (request, response) => {
   // })
 }
 
+app.delete("/eventoid/:id", (request, response) => {
+  const {id} = request.params
+  //pool.query('delete from CrowdMeet.Evento e using CrowdMeet.Participantes p where p.evento = $1 and e.id = $1', [id], (error, results) => {
+  pool.query('delete from CrowdMeet.Evento where id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({status: 'success', message: 'Evento apagado.'})
+  })
+})
+
+app.delete("/participantesevento/:id", (request, response) => {
+  const {id} = request.params
+  pool.query('delete from CrowdMeet.participantes where evento = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({status: 'success', message: 'Evento apagado.'})
+  })
+})
+
 const getEvento = (request, response) => {
   const {id} = request.body
   if(id == null){
@@ -75,10 +102,10 @@ const getEvento = (request, response) => {
 }
 
 const updateEvento = (request, response) => {
-  const {id, localizacao, datahora, tipoevento, subtipo} = request.body
+  const {id, localizacao, datahora, tipoevento} = request.body
   pool.query('update CrowdMeet.evento set ' + 
-  'localizacao = $1, tipoevento = $2, subtipo = $3, datahora = $4 where id = $5',
-  [localizacao, tipoevento, subtipo, datahora, id], (error, results) => {
+  'localizacao = $1, tipoevento = $2, datahora = $3 where id = $4',
+  [localizacao, tipoevento, datahora, id], (error, results) => {
     if (error) {
       throw error
     }
@@ -127,25 +154,25 @@ const getTipo = (request, response) => {
   }
 }
 
-const getSubtipo = (request, response) => {
-  const {id} = request.body
-  if(id == null){
-    pool.query('SELECT * FROM CrowdMeet.Subtipoevento', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
-  else{
-    pool.query('SELECT * FROM CrowdMeet.Subtipoevento where id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
-}
+// const getSubtipo = (request, response) => {
+//   const {id} = request.body
+//   if(id == null){
+//     pool.query('SELECT * FROM CrowdMeet.Subtipoevento', (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).json(results.rows)
+//     })
+//   }
+//   else{
+//     pool.query('SELECT * FROM CrowdMeet.Subtipoevento where id = $1', [id], (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).json(results.rows)
+//     })
+//   }
+// }
 
 const getDepartamento = (request, response) => {
   const {id} = request.body
@@ -219,7 +246,7 @@ app.route('/participante').get(getParticipante).post(addParticipante)
 
 app.route('/tipo').get(getTipo)
 
-app.route('/subtipo').get(getSubtipo)
+// app.route('/subtipo').get(getSubtipo)
 
 app.route('/departamento').get(getDepartamento)
 
